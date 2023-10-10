b57ebcf22d5c922c9d544b6a01ce11078cc6c7ef
shin-dongheon <tlsejdgjs@naver.com>
2023-10-10 14:01:18 +0900

    @SpringBootApplocation: @SpringBootConfiguration

--- springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.poscodx.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 /**
@@ -17,15 +18,37 @@ public class HelloWorldApplication {
          * 1. 애플리케이션 컨텍스트 ( Application Context, Spring Container) 생성, 컨터이너가 있으면 당연 설정파일이 있어야함 -> HellowroldApplication
          *
          * 2. 어노테이션 스캐닝(Component Scanning) + Configuration Class 를 통한
-         *      빈 생성 및 등록 작업
+         *      빈 생성, 등록, 와이어링 작업
          *
          * 3. 만약, 웹 애플리케이션 이라면, 웹 애플리케이션 타입을 결정(spring MVC, Reactive)
          * 4. 만약, 웹 애플리케이션 이고 타입이 SpringMVC 이면,
-         *      - 내장(embedded) 서버 (TomcatEmbededServiceServletContainer)
-         *      - 서버 인스턴스 웹 애플리케이션 배포
+         *      - 내장(embedded) 서버 (TomcatEmbededServiceServletContainer) 인스턴스 생성
+         *      - 서버 인스턴스 웹 애플리케이션 배포(mvc, aop, security, jpa, ... auto Configuration)
          *      - 서버 인스턴스 스타트
          * 5. ApplicationRunner 인터페이스 구현한 빈을 Application Context 찾아서 실행
          */
-        SpringApplication.run(HelloWorldApplication.class, args);
+//        ConfigurableApplicationContext ac = null;
+//        try {
+//             ac = SpringApplication.run(HelloWorldApplication.class, args);
+//        } catch (Throwable ex){
+//            ex.printStackTrace();
+//        }finally {
+//            if (ac != null){
+//                ac.close();
+//            }
+//        }
+        
+        // try ~ with ~ resources 구문
+        try( ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)) {
+            //web application인 경우
+            //SpringApplication.run(HelloWorldApplication.class, args);
+
+            System.out.println("some code....");
+            System.out.println("some code....");
+            System.out.println("some code....");
+            System.out.println("some code....");
+            System.out.println("some code....");
+        }
+
     }
 }
